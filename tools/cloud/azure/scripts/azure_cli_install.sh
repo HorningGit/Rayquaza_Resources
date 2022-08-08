486a374170b6bb7c15d90636f6ad20f734f0582f
AlexanderHorning <42924284+HorningGit@users.noreply.github.com>
2022-08-08 14:38:58 -0500

    Azure CLI Installation Script create

---
+++ tools/cloud/azure/scripts/azure_cli_install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+
+###################################################################
+# Script Name   : azure_cli_install.sh
+# Description   : Installation of Azure CLI
+# Author        : Alexander Horning
+# Email         : Alex.Horning@gmail.com
+###################################################################
+
