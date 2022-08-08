333f0e6651df2d6610b5dc95978bba08fb12ddea
AlexanderHorning <42924284+HorningGit@users.noreply.github.com>
2022-08-08 15:01:43 -0500

    Update AZURECLI_Install_CENTOS7.sh

--- tools/cloud/azure/scripts/AZURECLI_Install_CENTOS7.sh
+++ tools/cloud/azure/scripts/AZURECLI_Install_CENTOS7.sh
@@ -14,10 +14,8 @@ set -o pipefail
 # Import the Microsoft repository key.
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
 
-# Add the _packages-microsoft-prod_ repository into _/etc/yum.repos.d/_
-dnf install -y https://packages.microsoft.com/config/rhel/8/packages-microsoft-prod.rpm
-
-# 
+# Add the _packages-microsoft-prod_ repository into _/etc/yum.repos.d/ 
 echo -e "[azure-cli] name=Azure CLI baseurl=https://packages.microsoft.com/yumrepos/azure-cli enabled=1 gpgcheck=1 gpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/azure-cli.repo 
 
+# Install Azure CLI with dnf
 sudo dnf install azure-cli
